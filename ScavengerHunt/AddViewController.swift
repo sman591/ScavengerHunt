56c784a200d6210d8ad2d1a3b13c2ce3fbe28c5b
Stuart Olivera <stuart@stuartolivera.com>
2015-02-02 22:06:29 -0500

    Enabled add functionality

--- ScavengerHunt/AddViewController.swift
+++ ScavengerHunt/AddViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class AddViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    var newItem: ScavengerHuntItem?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,5 +28,14 @@ class AddViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "doneItemSegue" {
+            if let name = textField.text {
+                if !name.isEmpty {
+                    newItem = ScavengerHuntItem(name: textField.text)
+                }
+            }
+        }
+    }
 
 }
--- ScavengerHunt/ListViewController.swift
+++ ScavengerHunt/ListViewController.swift
@@ -11,6 +11,17 @@ import UIKit
 class ListViewController:UITableViewController {
     var itemsList = [ScavengerHuntItem(name: "Cat"), ScavengerHuntItem(name: "Bird"), ScavengerHuntItem(name: "Brick")]
     
+    @IBAction func unwindToList(segue: UIStoryboardSegue) {
+        if segue.identifier == "doneItemSegue" {
+            let addItemController = segue.sourceViewController as AddViewController
+            if let newItem = addItemController.newItem {
+                itemsList += [newItem]
+                let indexPath = NSIndexPath(forRow: itemsList.count-1, inSection: 0)
+                tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+            }
+        }
+    }
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemsList.count
     }
